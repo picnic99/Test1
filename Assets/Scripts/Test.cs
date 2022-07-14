67c8b47293358ca30c59ff4a786a7dcfabe14cef
hongyaoyong <hongyaoyong@dobest.com>
2022-07-14 10:13:03 +0800

    第一次提交

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    public Text t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t.text = "KIKI";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
